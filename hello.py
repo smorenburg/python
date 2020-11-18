ca5bbcca53493a2a653df098fabafdd70694057a
Robin Smorenburg <robin@smorenburg.io>
2020-11-18 11:46:26 +0100

    Add first files

---
+++ hello.py
@@ -0,0 +1,6 @@
+# print("Hello Rayen!")
+
+print("What is your name?")
+name = input()
+
+print("Hello, " + name)
---
+++ numbers.py
@@ -0,0 +1,8 @@
+print("First number:")
+first_number = int(input())
+
+print("Second number:")
+second_number = int(input())
+
+sum = first_number + second_number
+print("Sum: " + str(sum))
