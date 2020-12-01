8a53c06d728c19fd292b96ebb0d1f091dac46b0b
Robin Smorenburg <robin@smorenburg.io>
2020-12-01 21:17:51 +0100

    New exercise files

---
+++ source/brilliant/star.py
@@ -0,0 +1,8 @@
+from turtle import *
+color('red')
+
+for i in range(20):
+    forward(200)
+    left(108)
+
+bye()
---
+++ source/brilliant/turtle.py
@@ -0,0 +1,12 @@
+from turtle import *
+
+forward(200)
+
+# Make a diameter 200 semicircle, curving down
+for i in range(100):
+    forward(3.1416)
+    right(1.8)
+
+forward(200)
+
+bye()
