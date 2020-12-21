92ae8b894923fe4b9a1a50815a01979f285e2535
Robin Smorenburg <robin@smorenburg.io>
2020-12-21 15:39:07 +0100

    Optimize syntax

--- source/pgbackup/source/pgbackup/cli.py
+++ source/pgbackup/source/pgbackup/cli.py
@@ -4,10 +4,18 @@ known_drivers = ['local', 's3']
 
 
 class DriverAction(Action):
-    def __call__(self, parser, namespace, values, option_string=None):
+    def __call__(
+        self,
+        parser,
+        namespace,
+        values,
+        option_string=None
+    ):
         driver, destination = values
         if driver.lower() not in known_drivers:
-            parser.error("Unknown driver. Available drivers are 'local & 's3'")
+            parser.error(
+                "Unknown driver. Available drivers are 'local and 's3'"
+            )
         namespace.driver = driver.lower()
         namespace.destination = destination
 
@@ -21,7 +29,7 @@ def create_parser():
     parser.add_argument(
         '--driver',
         '-d',
-        help='URL of the PostgreSQL database to backup',
+        help='how and where to store the backup',
         nargs=2,
         action=DriverAction,
         required=True
