b86e21cd88c9859fa331b886d788b59a0254f897
Robin Smorenburg <robin@smorenburg.io>
2021-01-05 12:17:31 +0100

    Update course file

--- source/lists.py
+++ source/lists.py
@@ -14,3 +14,14 @@ while True:
 
 average = sum(numlist) / len(numlist)
 print('Average:', average)
+
+
+fhand = open('mbox-short.txt')
+for line in fhand:
+    line = line.rstrip()
+    if not line.startswith('From:'):
+        continue
+    words = line.split()
+    email = words[1]
+    pieces = email.split('@')
+    print('Domain name:', pieces[1])
