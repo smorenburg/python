74b72a8b078b22be392455981a01ac13b6f65cdc
Robin Smorenburg <robin@smorenburg.io>
2021-01-05 11:28:26 +0100

    Update course file

--- source/lists.py
+++ source/lists.py
@@ -2,3 +2,15 @@ friends = ['Robin', 'Ronak']
 for friend in friends:
     print('Happy new year:', friend)
 print('Done')
+
+
+numlist = list()
+while True:
+    inp = input('Enter a number: ')
+    if inp == 'done':
+        break
+    value = float(inp)
+    numlist.append(value)
+
+average = sum(numlist) / len(numlist)
+print('Average:', average)
