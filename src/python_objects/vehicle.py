8615902ffde7351821fecc4d079a49bb5ae3b656
Robin Smorenburg <robin@smorenburg.io>
2021-01-09 13:56:49 +0100

    Update course file

--- src/python_objects/vehicle.py
+++ src/python_objects/vehicle.py
@@ -3,28 +3,20 @@ class Vehicle:
     Vehicle is a type that describes a machine that helps us travel.
     """
 
-    default_tire = 'tire'
+    def __init__(self, distance_traveled=0, unit='kilometers', **kwargs):
+        self.distance_traveled = distance_traveled
+        self.unit = unit
 
-    def __init__(self, engine, tires):
-        """
-        Customizes the initialization of the object.
-        """
-        self.engine = engine
-        self.tires = tires
-
-    @classmethod
-    def bicycle(cls, tires=None):
-        """
-        """
-        if not tires:
-            tires = [cls.default_tire, cls.default_tire]
-        return cls(None, tires)
+    # @classmethod
+    # def bicycle(cls, tires=None):
+    #     """
+    #     """
+    #     if not tires:
+    #         tires = [cls.default_tire, cls.default_tire]
+    #     return cls(None, tires)
 
     def description(self):
-        """
-        Prints out the description of the vehicle.
-        """
-        print(
-            f'A vehicle with an {self.engine} engine, '
-            f'and {self.tires} tires'
+        return(
+            f'A {str(self.__class__.__name__).lower()} that has traveled '
+            f'{self.distance_traveled} {self.unit}'
         )
