bbdedf7625651274b91776637037ef547c647729
Robin Smorenburg <robin@smorenburg.io>
2021-01-10 14:46:34 +0100

    Update course file

--- src/file_io/using_files.py
+++ src/file_io/using_files.py
@@ -1,17 +1,19 @@
-my_file = open('xmen.txt', 'w+')
-my_file.write('Beast\n')
-my_file.write('Phoenix\n')
-my_file.writelines([
-    'Cyclops\n',
-    'Bishop\n',
-    'Nightcrawler\n'
-])
+with open('xmen.txt', 'w+') as my_file:
+    my_file.write('Beast\n')
+    my_file.write('Phoenix\n')
+    my_file.writelines([
+        'Cyclops\n',
+        'Bishop\n',
+        'Nightcrawler\n'
+    ])
 
-my_file.seek(0)
-print(my_file.read())
+    my_file.seek(0)
+    my_file.write('Morph')
+    my_file.seek(0)
 
-my_file.close()
+    for line in my_file.readlines():
+        print(line)
 
 # my_file = open('xmen.txt', 'r')
-# print(my_file.read())
-# my_file.close()
+# with my_file:
+#     print(my_file.read())
