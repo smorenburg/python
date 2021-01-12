8cb223a5de93e5d23f5a4c512574382821de0306
Robin Smorenburg <robin@smorenburg.io>
2021-01-12 11:53:07 +0100

    Modify course file

--- src/generate_list.py
+++ src/generate_list.py
@@ -1,4 +1,4 @@
-def generate_list(start, end, step):
+def generate_list(start, stop, step):
     """
     >>> generate_list(0, 5, 1)
     [0, 1, 2, 3, 4]
@@ -12,13 +12,27 @@ def generate_list(start, end, step):
     idx = start
     lst = []
 
-    if idx < end:
-        while idx < end:
+    if idx < stop:
+        while idx < stop:
             lst.append(idx)
             idx += step
     else:
-        while idx > end:
+        while idx > stop:
             lst.append(idx)
             idx += step
 
     return lst
+
+
+def generate_list_range(start, stop, step):
+    """
+    >>> generate_list(0, 5, 1)
+    [0, 1, 2, 3, 4]
+    >>> generate_list(0, 0, 1)
+    []
+    >>> generate_list(5, 10, 2)
+    [5, 7, 9]
+    >>> generate_list(10, 5, -2)
+    [10, 8, 6]
+    """
+    return list(range(start, stop, step))
