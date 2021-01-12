2a3910f66025e8e19efbe2a24e31de9be466c825
Robin Smorenburg <robin@smorenburg.io>
2021-01-12 15:56:18 +0100

    Modify course file

--- src/validate_subsequence.py
+++ src/validate_subsequence.py
@@ -1,42 +1,42 @@
-def validate_subsequence_for_loop(array, sequence):
+def validate_subsequence_for_loop(arr, seq):
     """
-    >>> array = [5, 1, 22, 25, 6, -1, 8, 10]
-    >>> sequence = [1, 6, -1, 10]
-    >>> validate_subsequence_for_loop(array, sequence)
+    >>> arr = [5, 1, 22, 25, 6, -1, 8, 10]
+    >>> seq = [1, 6, -1, 10]
+    >>> validate_subsequence_for_loop(arr, seq)
     True
-    >>> array = [5, 1, 22, 25, 6, -1, 8, 10]
-    >>> sequence = [1, 6, 2, 10]
-    >>> validate_subsequence_for_loop(array, sequence)
+    >>> arr = [5, 1, 22, 25, 6, -1, 8, 10]
+    >>> seq = [1, 6, 2, 10]
+    >>> validate_subsequence_for_loop(arr, seq)
     False
     """
     seq_idx = 0
 
-    for value in array:
-        if seq_idx == len(sequence):
+    for item in arr:
+        if seq_idx == len(seq):
             break
-        if sequence[seq_idx] == value:
+        if seq[seq_idx] == item:
             seq_idx += 1
 
-    return seq_idx == len(sequence)
+    return seq_idx == len(seq)
 
 
-def validate_subsequence_while_loop(array, sequence):
+def validate_subsequence_while_loop(arr, seq):
     """
-    >>> array = [5, 1, 22, 25, 6, -1, 8, 10]
-    >>> sequence = [1, 6, -1, 10]
-    >>> validate_subsequence_for_loop(array, sequence)
+    >>> arr = [5, 1, 22, 25, 6, -1, 8, 10]
+    >>> seq = [1, 6, -1, 10]
+    >>> validate_subsequence_for_loop(arr, seq)
     True
-    >>> array = [5, 1, 22, 25, 6, -1, 8, 10]
-    >>> sequence = [1, 6, 2, 10]
-    >>> validate_subsequence_for_loop(array, sequence)
+    >>> arr = [5, 1, 22, 25, 6, -1, 8, 10]
+    >>> seq = [1, 6, 2, 10]
+    >>> validate_subsequence_for_loop(arr, seq)
     False
     """
     arr_idx = 0
     seq_idx = 0
 
-    while arr_idx < len(array) and seq_idx < len(sequence):
-        if array[arr_idx] == sequence[seq_idx]:
+    while arr_idx < len(arr) and seq_idx < len(seq):
+        if arr[arr_idx] == seq[seq_idx]:
             seq_idx += 1
         arr_idx += 1
 
-    return seq_idx == len(sequence)
+    return seq_idx == len(seq)
