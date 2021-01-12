f1319ea72f7b7aba9534dc07c4bafec2115e36f0
Robin Smorenburg <robin@smorenburg.io>
2021-01-12 19:05:51 +0100

    Add course file

---
+++ src/recursion.py
@@ -0,0 +1,105 @@
+def func_r(a):
+    print(f'Start: {str(a)}')
+    res = 0
+    if a == 0:
+        res = 0
+    else:
+        res = func_r(a - 1) + 1
+    print(f'End: {str(a)}')
+    return res
+
+
+# f(0) = f(1) = f(n) = f(n - 1) + F(n - 2)
+def fib(n):
+    """
+    >>> fib(5)
+    5
+    >>> fib(0)
+    0
+    >>> fib(10)
+    55
+    >>> fib(20)
+    6765
+    """
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+def reverse(string):
+    """
+    >>> reverse('Robin')
+    'niboR'
+    """
+    if len(string) < 2:
+        return string
+    else:
+        return reverse(string[1:]) + string[0]
+
+
+def print_list(lst):
+    """
+    >>> print_list([1, 2, 3, 4, 5])
+    1
+    2
+    3
+    4
+    5
+    """
+    if not lst:
+        return
+    else:
+        print(lst[0])
+        print_list(lst[1:])
+
+
+def is_in_list(num, lst):
+    """
+    >>> is_in_list(4, [1, 2, 3, 4, 5])
+    True
+    >>> is_in_list(6, [1, 2, 3, 4, 5])
+    False
+    >>> is_in_list(5, [5, 4, 3, 2, 1])
+    True
+    """
+    if not lst:
+        return False
+    else:
+        return lst[0] == num or is_in_list(num, lst[1:])
+
+
+def is_increasing(lst):
+    """
+    >>> is_increasing([1, 2, 3, 4])
+    True
+    >>> is_increasing([1, 3, 2, 4])
+    False
+    """
+    if not lst:
+        return False
+    elif len(lst) == 1:
+        return True
+    elif lst[1] > lst[0]:
+        return is_increasing(lst[1:])
+    else:
+        return False
+
+
+def filter_gt_num(lst, num):
+    """
+    >>> filter_gt_num([1, 2, 3, 4], 2)
+    [3, 4]
+    >>> filter_gt_num([2, 2, 3, 3], 1)
+    [2, 2, 3, 3]
+    >>> filter_gt_num([2, 2, 3, 3], 4)
+    []
+    """
+    if not lst:
+        return []
+    elif lst[0] > num:
+        return [lst[0]] + filter_gt_num(lst[1:], num)
+    else:
+        return filter_gt_num(lst[1:], num)
